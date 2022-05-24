3fbf125962d4918e9c2b93e82ba06e351acb4b68
Julio <juliocesarcs2004@gmail.com>
2022-05-24 18:59:53 -0300

    Parte 1 Finalizada

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit{
+
+    courses: Course[] = [];
+
+    constructor(private courseService: CourseService) { }
+
+    ngOnInit(): void {
+        this.courses = this.courseService.retrieveAll();
+    }
+
+}
