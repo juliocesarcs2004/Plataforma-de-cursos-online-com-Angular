ccf3b94b234a8992899a3ff3165d564eb11dda2a
Julio <juliocesarcs2004@gmail.com>
2022-05-28 12:04:28 -0300

    Concluído Item: Ativando Rotas para Acessar Componentes

---
+++ src/app/courses/course-info.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+    templateUrl: './course-info.component.html'
+})
+export class CourseInfoComponent implements OnInit {
+
+    courseId!: number;
+
+    constructor(private activatedRoute: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        this.courseId = +this.activatedRoute.snapshot.paramMap.get!('id')!;
+    }
+}
