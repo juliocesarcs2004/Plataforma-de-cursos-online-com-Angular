afe5e7256baab68c591146838da1bcc622d2736f
Julio <juliocesarcs2004@gmail.com>
2022-05-31 10:17:35 -0300

    Parte 2 Finalizada

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     templateUrl: './course-info.component.html'
 })
 export class CourseInfoComponent implements OnInit {
 
-    courseId!: number;
+    course!: Course;
 
-    constructor(private activatedRoute: ActivatedRoute) {}
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
 
     ngOnInit(): void {
-        this.courseId = +this.activatedRoute.snapshot.paramMap.get!('id')!;
+        this.course= this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get!('id')!);
     }
+
+    save(): void{
+        this.courseService.save(this.course);
+    }
+
 }
