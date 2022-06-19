6880144dd803316726a5ce7279c2632d7c70452b
Julio <juliocesarcs2004@gmail.com>
2022-06-19 10:11:15 -0300

    Requisições Http implementadas com o back-end em Node.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
@@ -25,6 +27,7 @@ import { CourseInfoComponent } from './courses/course-info.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
         path: 'courses', component: CourseListComponent
--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -13,11 +13,17 @@ export class CourseInfoComponent implements OnInit {
     constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
 
     ngOnInit(): void {
-        this.course= this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get!('id')!);
+        this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')!).subscribe({
+            next: course => this.course = course,
+            error: err => console.log('Error', err)
+        }); 
     }
 
     save(): void{
-        this.courseService.save(this.course);
+        this.courseService.save(this.course).subscribe({
+            next: course => console.log('Saved with success', course),
+            error: err => console.log('Error', err)
+        });
     }
 
 }
