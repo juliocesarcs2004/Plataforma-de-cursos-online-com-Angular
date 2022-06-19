755a848f2e9df8ed9b407d281741a826f69eae1f
Julio <juliocesarcs2004@gmail.com>
2022-06-19 11:01:32 -0300

    Método Delete acrescentado

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -29,6 +29,16 @@ export class CourseListComponent implements OnInit{
         })
     }
 
+    deleteById(courseId: number): void {
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => {
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
+    }
+
     set filter(value: string){
         this._filterBy = value;
 
--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -29,6 +29,10 @@ export class CourseService {
         }
     }
 
+    deleteById(id: number): Observable<any> {
+        return this.httpClient.delete<any>(`${this.coursesUrl}/${id}`);
+    }
+
 }
 
 var COURSES: Course[] = [
